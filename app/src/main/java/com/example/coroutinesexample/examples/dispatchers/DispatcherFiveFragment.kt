626e9207f27f021bc9c719482a01824b52e083bb
a1u2m <xxxla1u2mlxxx@gmail.com>
2023-08-20 12:01:54 +0500

    dispatchers case 5

--- app/src/main/java/com/example/coroutinesexample/examples/dispatchers/DispatcherFiveFragment.kt
+++ app/src/main/java/com/example/coroutinesexample/examples/dispatchers/DispatcherFiveFragment.kt
@@ -7,6 +7,13 @@ import android.view.ViewGroup
 import com.example.coroutinesexample.R
 import com.example.coroutinesexample.databinding.FragmentDispatcherFiveBinding
 import com.example.coroutinesexample.examples.BaseExampleFragment
+import com.example.coroutinesexample.utis.Utils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.concurrent.TimeUnit
 
 class DispatcherFiveFragment : BaseExampleFragment() {
 
@@ -22,5 +29,114 @@ class DispatcherFiveFragment : BaseExampleFragment() {
     ): View {
         binding = FragmentDispatcherFiveBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonUnconfined.setOnClickListener {
+            unconfinedAction()
+        }
+        binding.buttonMain.setOnClickListener {
+            mainAction()
+        }
+    }
+
+    private fun unconfinedAction() {
+        CoroutineScope(Dispatchers.Unconfined).launch {
+            loggerVM.addLog(
+                resources.getString(
+                    R.string.dispatchersCase5Action1,
+                    Thread.currentThread().name,
+                    Utils.getCurrentTime()
+                )
+            )
+            actionBody()
+            loggerVM.addLog(
+                resources.getString(
+                    R.string.dispatchersCase5Action9
+                )
+            )
+        }
+    }
+
+    private fun mainAction() {
+        CoroutineScope(Dispatchers.Main).launch {
+            loggerVM.addLog(
+                resources.getString(
+                    R.string.dispatchersCase5Action11,
+                    Thread.currentThread().name,
+                    Utils.getCurrentTime()
+                )
+            )
+            actionBody()
+            loggerVM.addLog(
+                resources.getString(
+                    R.string.dispatchersCase5Action10
+                )
+            )
+        }
+    }
+
+    private suspend fun actionBody() {
+        TimeUnit.MILLISECONDS.sleep(1000)
+        loggerVM.addLog(
+            resources.getString(
+                R.string.dispatchersCase5Action2,
+                Thread.currentThread().name,
+                Utils.getCurrentTime()
+            )
+        )
+        unconfinedSuspendTest()
+        loggerVM.addLog(
+            resources.getString(
+                R.string.dispatchersCase5Action8,
+                Thread.currentThread().name,
+                Utils.getCurrentTime()
+            )
+        )
+        TimeUnit.MILLISECONDS.sleep(1000)
+        loggerVM.addLog(
+            resources.getString(
+                R.string.dispatchersCase5Action3,
+                Thread.currentThread().name,
+                Utils.getCurrentTime()
+            )
+        )
+    }
+
+    private suspend fun unconfinedSuspendTest() {
+        loggerVM.addLog(
+            resources.getString(
+                R.string.dispatchersCase5Action4,
+                Thread.currentThread().name,
+                Utils.getCurrentTime()
+            )
+        )
+        delay(1000)
+        loggerVM.addLog(
+            resources.getString(
+                R.string.dispatchersCase5Action5,
+                Thread.currentThread().name,
+                Utils.getCurrentTime()
+            )
+        )
+        delay(1000)
+        withContext(Dispatchers.IO) {
+            loggerVM.addLog(
+                resources.getString(
+                    R.string.dispatchersCase5Action6,
+                    Thread.currentThread().name,
+                    Utils.getCurrentTime()
+                )
+            )
+        }
+        delay(1000)
+        loggerVM.addLog(
+            resources.getString(
+                R.string.dispatchersCase5Action7,
+                Thread.currentThread().name,
+                Utils.getCurrentTime()
+            )
+        )
     }
 }
