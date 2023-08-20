9de71102ba13b0100161cfe5ae17dd8916edf4a8
a1u2m <xxxla1u2mlxxx@gmail.com>
2023-08-20 10:55:14 +0500

    dispatchers case 3

--- app/src/main/java/com/example/coroutinesexample/examples/dispatchers/DispatcherThreeFragment.kt
+++ app/src/main/java/com/example/coroutinesexample/examples/dispatchers/DispatcherThreeFragment.kt
@@ -1,15 +1,19 @@
 package com.example.coroutinesexample.examples.dispatchers
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.MutableLiveData
 import com.example.coroutinesexample.R
 import com.example.coroutinesexample.databinding.FragmentDispatcherThreeBinding
 import com.example.coroutinesexample.examples.BaseExampleFragment
+import com.example.coroutinesexample.utis.Utils
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
 import java.util.concurrent.TimeUnit
 
@@ -19,6 +23,11 @@ class DispatcherThreeFragment : BaseExampleFragment() {
         resources.getString(R.string.dispatchersCase3)
     }
     lateinit var binding: FragmentDispatcherThreeBinding
+    private val scope = CoroutineScope(Dispatchers.IO)
+    private var allJobsCompleted = false
+    private val liveData = MutableLiveData<String>().apply {
+        this.value = scope.coroutineContext[Job.Key]?.children?.count().toString()
+    }
 
 
     override fun onCreateView(
@@ -29,5 +38,53 @@ class DispatcherThreeFragment : BaseExampleFragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener {
+            action()
+        }
+        liveData.observe(viewLifecycleOwner) {
+            binding.counter.text = it
+        }
+    }
 
+    private fun action() {
+        repeat(100) {
+            val job = scope.launch {
+                liveData.postValue(
+                    scope.coroutineContext.job.children.filter { isActive }.count().toString())
+                loggerVM.addLog(
+                    resources.getString(
+                        R.string.dispatchersCase3Action1,
+                        it,
+                        Thread.currentThread().name,
+                        Utils.getCurrentTime()
+                    )
+                )
+                TimeUnit.MILLISECONDS.sleep(5000)
+                loggerVM.addLog(
+                    resources.getString(
+                        R.string.dispatchersCase3Action2,
+                        it,
+                        Thread.currentThread().name,
+                        Utils.getCurrentTime()
+                    )
+                )
+                liveData.postValue(scope.coroutineContext.job.children.filter { isActive }.count().toString())
+            }
+            job.invokeOnCompletion { throwable ->
+                if (scope.coroutineContext.isActive && !allJobsCompleted) {
+                    if (throwable == null) {
+                        if (scope.coroutineContext.job.children.count { it.isActive } == 0) {
+                            allJobsCompleted = true
+                            scope.launch {
+                                loggerVM.addLog(resources.getString(R.string.dispatchersCase3Action3))
+                            }
+                        }
+                    }
+                }
+                liveData.postValue(scope.coroutineContext.job.children.count().toString())
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/coroutinesexample/examples/dispatchers/DispatcherTwoFragment.kt
+++ app/src/main/java/com/example/coroutinesexample/examples/dispatchers/DispatcherTwoFragment.kt
@@ -91,6 +91,5 @@ class DispatcherTwoFragment : BaseExampleFragment() {
                 }
             }
         }
-
     }
 }
