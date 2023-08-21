9cb60d3f792a5c6be2a532ea23aaf59ca1ff34f0
a1u2m <xxxla1u2mlxxx@gmail.com>
2023-08-21 10:37:22 +0500

    builders case 1

--- app/src/main/java/com/example/coroutinesexample/examples/builders/BuilderOneFragment.kt
+++ app/src/main/java/com/example/coroutinesexample/examples/builders/BuilderOneFragment.kt
@@ -7,6 +7,11 @@ import android.view.ViewGroup
 import com.example.coroutinesexample.R
 import com.example.coroutinesexample.databinding.FragmentBuilderOneBinding
 import com.example.coroutinesexample.examples.BaseExampleFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class BuilderOneFragment : BaseExampleFragment() {
 
@@ -14,6 +19,7 @@ class BuilderOneFragment : BaseExampleFragment() {
         resources.getString(R.string.buildersCase1)
     }
     lateinit var binding: FragmentBuilderOneBinding
+    val scope = CoroutineScope(Job())
 
 
     override fun onCreateView(
@@ -22,5 +28,28 @@ class BuilderOneFragment : BaseExampleFragment() {
     ): View {
         binding = FragmentBuilderOneBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener {
+            action()
+        }
+    }
+
+    private fun action() {
+        scope.launch {
+            loggerVM.addLog(resources.getString(R.string.buildersCase1Action1))
+            val deferred = async {
+                loggerVM.addLog(resources.getString(R.string.buildersCase1Action2))
+                delay(1000)
+                loggerVM.addLog(resources.getString(R.string.buildersCase1Action3))
+                resources.getString(R.string.buildersCase1Action4)
+            }
+            loggerVM.addLog(resources.getString(R.string.buildersCase1Action5))
+            val result = deferred.await()
+            loggerVM.addLog(resources.getString(R.string.buildersCase1Action6, result))
+            loggerVM.addLog(resources.getString(R.string.buildersCase1Action7))
+        }
     }
 }
