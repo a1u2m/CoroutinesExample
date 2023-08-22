208e9df90e57d42a79da57d987482dbef0261ea1
a1u2m <xxxla1u2mlxxx@gmail.com>
2023-08-22 17:32:55 +0500

    flows case 4

--- app/src/main/java/com/example/coroutinesexample/examples/flows/FlowFourFragment.kt
+++ app/src/main/java/com/example/coroutinesexample/examples/flows/FlowFourFragment.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.example.coroutinesexample.R
 import com.example.coroutinesexample.databinding.FragmentFlowFourBinding
 import com.example.coroutinesexample.examples.BaseExampleFragment
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class FlowFourFragment : BaseExampleFragment() {
 
@@ -14,6 +20,7 @@ class FlowFourFragment : BaseExampleFragment() {
         resources.getString(R.string.flowCase4)
     }
     lateinit var binding: FragmentFlowFourBinding
+    private lateinit var flow: Flow<Int>
 
 
     override fun onCreateView(
@@ -22,5 +29,36 @@ class FlowFourFragment : BaseExampleFragment() {
     ): View {
         binding = FragmentFlowFourBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonCreate.setOnClickListener {
+            createFlow()
+        }
+        binding.buttonFirstSub.setOnClickListener {
+            lifecycleScope.launch {
+                subscribeFirstSub(1)
+            }
+        }
+    }
+
+    @OptIn(FlowPreview::class)
+    private fun createFlow() {
+        flow = ::generateNumber.asFlow()
+    }
+
+    private fun generateNumber(): Int {
+        return Random.nextInt()
+    }
+
+    private suspend fun subscribeFirstSub(number: Int) {
+        flow.collect { i ->
+            doWork(i)
+        }
+    }
+
+    private fun doWork(i: Int) {
+        loggerVM.addLog("$i")
     }
 }
