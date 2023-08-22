c9f33cbca08d467553734dc20cc8353ddddf5104
a1u2m <xxxla1u2mlxxx@gmail.com>
2023-08-22 16:39:32 +0500

    flows case 1

--- app/src/main/java/com/example/coroutinesexample/examples/flows/FlowOneFragment.kt
+++ app/src/main/java/com/example/coroutinesexample/examples/flows/FlowOneFragment.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.example.coroutinesexample.R
 import com.example.coroutinesexample.databinding.FragmentFlowOneBinding
 import com.example.coroutinesexample.examples.BaseExampleFragment
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class FlowOneFragment : BaseExampleFragment() {
 
@@ -14,7 +19,7 @@ class FlowOneFragment : BaseExampleFragment() {
         resources.getString(R.string.flowCase1)
     }
     lateinit var binding: FragmentFlowOneBinding
-
+    private lateinit var flow: Flow<Int>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +27,58 @@ class FlowOneFragment : BaseExampleFragment() {
     ): View {
         binding = FragmentFlowOneBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonCreate.setOnClickListener {
+            createFlow()
+        }
+        binding.buttonFirstSub.setOnClickListener {
+            lifecycleScope.launch {
+                subscribeFirstSub(1)
+            }
+        }
+        binding.buttonSecondSub.setOnClickListener {
+            lifecycleScope.launch {
+                subscribeSecondSub(2)
+            }
+        }
+        binding.buttonThirdSub.setOnClickListener {
+            lifecycleScope.launch {
+                subscribeThirdSub(3)
+            }
+        }
+    }
+
+    private fun createFlow() {
+        flow = flow {
+            for (i in 1..10) {
+                delay(1000)
+                emit(i)
+            }
+        }
+    }
+
+    private suspend fun subscribeFirstSub(number: Int) {
+        flow.collect { i ->
+            doWork(number, i)
+        }
+    }
+
+    private suspend fun subscribeSecondSub(number: Int) {
+        flow.collect { i ->
+            doWork(number, i)
+        }
+    }
+
+    private suspend fun subscribeThirdSub(number: Int) {
+        flow.collect { i ->
+            doWork(number, i)
+        }
+    }
+
+    private fun doWork(number: Int, i: Int) {
+        loggerVM.addLog(resources.getString(R.string.flowsCase1Action1, number, i))
     }
 }
--- app/src/main/java/com/example/coroutinesexample/home/HomeViewModel.kt
+++ app/src/main/java/com/example/coroutinesexample/home/HomeViewModel.kt
@@ -16,104 +16,104 @@ class HomeViewModel : ViewModel() {
         val temp = mutableListOf<HomeItemModel>()
         //закомменчиваю законченные блоки чтоб не мешались. потом верну
 
-//        temp.add(HomeItemModel.HomeTitleItem(R.string.titleDispatchers))
+//        temp.add(HomeItemModel.HomeTitleItem(R.string.titleDispatchers)) //gotovo
 //        temp.add(HomeItemModel.HomeItemItem(R.string.dispatchersCase1, R.id.toDispatchersOne))
 //        temp.add(HomeItemModel.HomeItemItem(R.string.dispatchersCase2, R.id.toDispatchersTwo))
 //        temp.add(HomeItemModel.HomeItemItem(R.string.dispatchersCase3, R.id.toDispatchersThree))
 //        temp.add(HomeItemModel.HomeItemItem(R.string.dispatchersCase4, R.id.toDispatchersFour))
 //        temp.add(HomeItemModel.HomeItemItem(R.string.dispatchersCase5, R.id.toDispatchersFive))
 
-        temp.add(HomeItemModel.HomeTitleItem(R.string.titleErrors))
-        temp.add(HomeItemModel.HomeItemItem(R.string.errorsCase1, R.id.toErrorsOne))
-        temp.add(HomeItemModel.HomeItemItem(R.string.errorsCase2, R.id.toErrorsTwo))
-        temp.add(HomeItemModel.HomeItemItem(R.string.errorsCase3, R.id.toErrorsThree))
-        temp.add(HomeItemModel.HomeItemItem(R.string.errorsCase4, R.id.toErrorsFour))
-        temp.add(HomeItemModel.HomeItemItem(R.string.errorsCase5, R.id.toErrorsFive))
-        temp.add(HomeItemModel.HomeItemItem(R.string.errorsCase6, R.id.toErrorsSix))
-        temp.add(HomeItemModel.HomeItemItem(R.string.errorsCase7, R.id.toErrorsSeven))
+//        temp.add(HomeItemModel.HomeTitleItem(R.string.titleErrors)) //ne gotovo
+//        temp.add(HomeItemModel.HomeItemItem(R.string.errorsCase1, R.id.toErrorsOne))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.errorsCase2, R.id.toErrorsTwo))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.errorsCase3, R.id.toErrorsThree))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.errorsCase4, R.id.toErrorsFour))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.errorsCase5, R.id.toErrorsFive))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.errorsCase6, R.id.toErrorsSix))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.errorsCase7, R.id.toErrorsSeven))
 
-//        temp.add(HomeItemModel.HomeTitleItem(R.string.titleBuilders))
+//        temp.add(HomeItemModel.HomeTitleItem(R.string.titleBuilders)) //gotovo
 //        temp.add(HomeItemModel.HomeItemItem(R.string.buildersCase1, R.id.toBuildersOne))
 //        temp.add(HomeItemModel.HomeItemItem(R.string.buildersCase2, R.id.toBuildersTwo))
 //        temp.add(HomeItemModel.HomeItemItem(R.string.buildersCase3, R.id.toBuildersThree))
 //        temp.add(HomeItemModel.HomeItemItem(R.string.buildersCase4, R.id.toBuildersFour))
 
-//        temp.add(HomeItemModel.HomeTitleItem(R.string.titleSuspend))
+//        temp.add(HomeItemModel.HomeTitleItem(R.string.titleSuspend)) //gotovo
 //        temp.add(HomeItemModel.HomeItemItem(R.string.suspendCase1, R.id.toSuspendOne))
 
-        temp.add(HomeItemModel.HomeTitleItem(R.string.titleCoroutines))
-        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase1, R.id.toCoroutinesOne))
-        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase2, R.id.toCoroutinesTwo))
-        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase3, R.id.toCoroutinesThree))
-        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase4, R.id.toCoroutinesFour))
-        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase5, R.id.toCoroutinesFive))
-        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase6, R.id.toCoroutinesSix))
-        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase7, R.id.toCoroutinesSeven))
-        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase8, R.id.toCoroutinesEight))
-        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase9, R.id.toCoroutinesNine))
-        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase10, R.id.toCoroutinesTen))
-        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase11, R.id.toCoroutinesEleven))
-        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase12, R.id.toCoroutinesTwelve))
-        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase13, R.id.toCoroutinesThirteen))
+//        temp.add(HomeItemModel.HomeTitleItem(R.string.titleCoroutines)) //ne gotovo
+//        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase1, R.id.toCoroutinesOne))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase2, R.id.toCoroutinesTwo))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase3, R.id.toCoroutinesThree))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase4, R.id.toCoroutinesFour))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase5, R.id.toCoroutinesFive))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase6, R.id.toCoroutinesSix))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase7, R.id.toCoroutinesSeven))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase8, R.id.toCoroutinesEight))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase9, R.id.toCoroutinesNine))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase10, R.id.toCoroutinesTen))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase11, R.id.toCoroutinesEleven))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase12, R.id.toCoroutinesTwelve))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.coroutinesCase13, R.id.toCoroutinesThirteen))
 
-        temp.add(HomeItemModel.HomeTitleItem(R.string.titleScope))
-        temp.add(HomeItemModel.HomeItemItem(R.string.scopeCase1, R.id.toScopeOne))
-        temp.add(HomeItemModel.HomeItemItem(R.string.scopeCase2, R.id.toScopeTwo))
-        temp.add(HomeItemModel.HomeItemItem(R.string.scopeCase3, R.id.toScopeThree))
-        temp.add(HomeItemModel.HomeItemItem(R.string.scopeCase4, R.id.toScopeFour))
+//        temp.add(HomeItemModel.HomeTitleItem(R.string.titleScope)) //ne gotovo
+//        temp.add(HomeItemModel.HomeItemItem(R.string.scopeCase1, R.id.toScopeOne))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.scopeCase2, R.id.toScopeTwo))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.scopeCase3, R.id.toScopeThree))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.scopeCase4, R.id.toScopeFour))
 
-        temp.add(HomeItemModel.HomeTitleItem(R.string.titleScopeFunctions))
-        temp.add(HomeItemModel.HomeItemItem(R.string.scopeFunctionsCase1, R.id.toScopeFunctionsOne))
-        temp.add(HomeItemModel.HomeItemItem(R.string.scopeFunctionsCase2, R.id.toScopeFunctionsTwo))
-        temp.add(
-            HomeItemModel.HomeItemItem(
-                R.string.scopeFunctionsCase3,
-                R.id.toScopeFunctionsThree
-            )
-        )
-        temp.add(
-            HomeItemModel.HomeItemItem(
-                R.string.scopeFunctionsCase4,
-                R.id.toScopeFunctionsFour
-            )
-        )
-        temp.add(
-            HomeItemModel.HomeItemItem(
-                R.string.scopeFunctionsCase5,
-                R.id.toScopeFunctionsFive
-            )
-        )
-        temp.add(HomeItemModel.HomeItemItem(R.string.scopeFunctionsCase6, R.id.toScopeFunctionsSix))
-        temp.add(
-            HomeItemModel.HomeItemItem(
-                R.string.scopeFunctionsCase7,
-                R.id.toScopeFunctionsSeven
-            )
-        )
-        temp.add(
-            HomeItemModel.HomeItemItem(
-                R.string.scopeFunctionsCase8,
-                R.id.toScopeFunctionsEight
-            )
-        )
+//        temp.add(HomeItemModel.HomeTitleItem(R.string.titleScopeFunctions)) //ne gotovo
+//        temp.add(HomeItemModel.HomeItemItem(R.string.scopeFunctionsCase1, R.id.toScopeFunctionsOne))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.scopeFunctionsCase2, R.id.toScopeFunctionsTwo))
+//        temp.add(
+//            HomeItemModel.HomeItemItem(
+//                R.string.scopeFunctionsCase3,
+//                R.id.toScopeFunctionsThree
+//            )
+//        )
+//        temp.add(
+//            HomeItemModel.HomeItemItem(
+//                R.string.scopeFunctionsCase4,
+//                R.id.toScopeFunctionsFour
+//            )
+//        )
+//        temp.add(
+//            HomeItemModel.HomeItemItem(
+//                R.string.scopeFunctionsCase5,
+//                R.id.toScopeFunctionsFive
+//            )
+//        )
+//        temp.add(HomeItemModel.HomeItemItem(R.string.scopeFunctionsCase6, R.id.toScopeFunctionsSix))
+//        temp.add(
+//            HomeItemModel.HomeItemItem(
+//                R.string.scopeFunctionsCase7,
+//                R.id.toScopeFunctionsSeven
+//            )
+//        )
+//        temp.add(
+//            HomeItemModel.HomeItemItem(
+//                R.string.scopeFunctionsCase8,
+//                R.id.toScopeFunctionsEight
+//            )
+//        )
 
-        temp.add(HomeItemModel.HomeTitleItem(R.string.titleChannels))
-        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase1, R.id.toChannelsOne))
-        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase2, R.id.toChannelsTwo))
-        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase3, R.id.toChannelsThree))
-        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase4, R.id.toChannelsFour))
-        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase5, R.id.toChannelsFive))
-        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase6, R.id.toChannelsSix))
-        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase7, R.id.toChannelsSeven))
-        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase8, R.id.toChannelsEight))
-        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase9, R.id.toChannelsNine))
-        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase10, R.id.toChannelsTen))
-        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase11, R.id.toChannelsEleven))
-        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase12, R.id.toChannelsTwelve))
-        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase13, R.id.toChannelsThirteen))
+//        temp.add(HomeItemModel.HomeTitleItem(R.string.titleChannels)) //ne gotovo
+//        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase1, R.id.toChannelsOne))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase2, R.id.toChannelsTwo))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase3, R.id.toChannelsThree))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase4, R.id.toChannelsFour))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase5, R.id.toChannelsFive))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase6, R.id.toChannelsSix))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase7, R.id.toChannelsSeven))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase8, R.id.toChannelsEight))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase9, R.id.toChannelsNine))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase10, R.id.toChannelsTen))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase11, R.id.toChannelsEleven))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase12, R.id.toChannelsTwelve))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.channelsCase13, R.id.toChannelsThirteen))
 
         temp.add(HomeItemModel.HomeTitleItem(R.string.titleFlow))
-        temp.add(HomeItemModel.HomeItemItem(R.string.flowCase1, R.id.toFlowOne))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.flowCase1, R.id.toFlowOne))
         temp.add(HomeItemModel.HomeItemItem(R.string.flowCase2, R.id.toFlowTwo))
         temp.add(HomeItemModel.HomeItemItem(R.string.flowCase3, R.id.toFlowThree))
         temp.add(HomeItemModel.HomeItemItem(R.string.flowCase4, R.id.toFlowFour))
@@ -140,9 +140,9 @@ class HomeViewModel : ViewModel() {
         temp.add(HomeItemModel.HomeItemItem(R.string.flowCase25, R.id.toFlowTwentyFive))
         temp.add(HomeItemModel.HomeItemItem(R.string.flowCase26, R.id.toFlowTwentySix))
 
-        temp.add(HomeItemModel.HomeTitleItem(R.string.titleOperators))
-        temp.add(HomeItemModel.HomeItemItem(R.string.operatorsCase1, R.id.toOperatorsOne))
-        temp.add(HomeItemModel.HomeItemItem(R.string.operatorsCase2, R.id.toOperatorsTwo))
+//        temp.add(HomeItemModel.HomeTitleItem(R.string.titleOperators)) //ne gotovo
+//        temp.add(HomeItemModel.HomeItemItem(R.string.operatorsCase1, R.id.toOperatorsOne))
+//        temp.add(HomeItemModel.HomeItemItem(R.string.operatorsCase2, R.id.toOperatorsTwo))
 
         _items.value = temp
     }
