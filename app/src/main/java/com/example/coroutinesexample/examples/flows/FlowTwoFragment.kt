3521884d21d97dbeedfb6ea27f8afbb39ab486b2
a1u2m <xxxla1u2mlxxx@gmail.com>
2023-08-22 17:06:06 +0500

    flows case 2

--- app/src/main/java/com/example/coroutinesexample/examples/flows/FlowTwoFragment.kt
+++ app/src/main/java/com/example/coroutinesexample/examples/flows/FlowTwoFragment.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.example.coroutinesexample.R
 import com.example.coroutinesexample.databinding.FragmentFlowTwoBinding
 import com.example.coroutinesexample.examples.BaseExampleFragment
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.launch
 
 class FlowTwoFragment : BaseExampleFragment() {
 
@@ -14,6 +19,7 @@ class FlowTwoFragment : BaseExampleFragment() {
         resources.getString(R.string.flowCase2)
     }
     lateinit var binding: FragmentFlowTwoBinding
+    private lateinit var flow: Flow<Int>
 
 
     override fun onCreateView(
@@ -22,5 +28,56 @@ class FlowTwoFragment : BaseExampleFragment() {
     ): View {
         binding = FragmentFlowTwoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonCreate.setOnClickListener {
+            createFlow()
+        }
+        binding.buttonFirstSub.setOnClickListener {
+            lifecycleScope.launch {
+                subscribeFirstSub(1)
+            }
+        }
+        binding.buttonSecondSub.setOnClickListener {
+            lifecycleScope.launch {
+                subscribeSecondSub(2)
+            }
+        }
+        binding.buttonThirdSub.setOnClickListener {
+            lifecycleScope.launch {
+                subscribeThirdSub(3)
+            }
+        }
+    }
+
+    private fun createFlow() {
+        flow = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).asFlow()
+    }
+
+    private suspend fun subscribeFirstSub(number: Int) {
+        flow.collect { i ->
+            doWork(number, i)
+            delay(1000)
+        }
+    }
+
+    private suspend fun subscribeSecondSub(number: Int) {
+        flow.collect { i ->
+            doWork(number, i)
+            delay(1000)
+        }
+    }
+
+    private suspend fun subscribeThirdSub(number: Int) {
+        flow.collect { i ->
+            doWork(number, i)
+            delay(1000)
+        }
+    }
+
+    private fun doWork(number: Int, i: Int) {
+        loggerVM.addLog(resources.getString(R.string.flowsCase1Action1, number, i))
     }
 }
