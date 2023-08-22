ac2aadc692b577670ea3b945c2b744bba71860ac
a1u2m <xxxla1u2mlxxx@gmail.com>
2023-08-22 11:54:05 +0500

    builders case 4

--- app/src/main/java/com/example/coroutinesexample/examples/builders/BuilderFourFragment.kt
+++ app/src/main/java/com/example/coroutinesexample/examples/builders/BuilderFourFragment.kt
@@ -4,9 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.example.coroutinesexample.R
 import com.example.coroutinesexample.databinding.FragmentBuilderFourBinding
 import com.example.coroutinesexample.examples.BaseExampleFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
 class BuilderFourFragment : BaseExampleFragment() {
 
@@ -14,6 +23,9 @@ class BuilderFourFragment : BaseExampleFragment() {
         resources.getString(R.string.buildersCase4)
     }
     lateinit var binding: FragmentBuilderFourBinding
+    private val scope = CoroutineScope(Dispatchers.Default)
+    lateinit var job: Job
+    lateinit var deferred: Deferred<Unit>
 
 
     override fun onCreateView(
@@ -22,5 +34,74 @@ class BuilderFourFragment : BaseExampleFragment() {
     ): View {
         binding = FragmentBuilderFourBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonLaunchCreate.setOnClickListener {
+            launchCreate()
+        }
+        binding.buttonLaunchStart.setOnClickListener {
+            launchStart()
+        }
+
+        binding.buttonAsyncCreate.setOnClickListener {
+            asyncCreate()
+        }
+        binding.buttonAsyncStart.setOnClickListener {
+            lifecycleScope.launch {
+                asyncStart()
+            }
+        }
+    }
+
+    private fun launchCreate() {
+        coroutineCreate()
+        job = scope.launch(start = CoroutineStart.LAZY) {
+            coroutineBody()
+        }
+        coroutineEndOfCreation()
+    }
+
+    private fun asyncCreate() {
+        coroutineCreate()
+        deferred = scope.async(start = CoroutineStart.LAZY) {
+            coroutineBody()
+        }
+        coroutineEndOfCreation()
+    }
+
+    private fun launchStart() {
+        coroutineStart()
+        job.start()
+        coroutineEnd()
+    }
+
+    private suspend fun asyncStart() {
+        coroutineStart()
+        deferred.await()
+        coroutineEnd()
+    }
+
+    private fun coroutineCreate() {
+        loggerVM.addLog(resources.getString(R.string.buildersCase4Action1))
+    }
+
+    private fun coroutineEndOfCreation() {
+        loggerVM.addLog(resources.getString(R.string.buildersCase4Action2))
+    }
+
+    private fun coroutineBody() {
+        loggerVM.addLog(resources.getString(R.string.buildersCase4Action3))
+        TimeUnit.MILLISECONDS.sleep(1000)
+        loggerVM.addLog(resources.getString(R.string.buildersCase4Action4))
+    }
+
+    private fun coroutineEnd() {
+        loggerVM.addLog(resources.getString(R.string.buildersCase4Action5))
+    }
+
+    private fun coroutineStart() {
+        loggerVM.addLog(resources.getString(R.string.buildersCase4Action6))
     }
 }
