4fd017ff641fde8bb4a075e51a677e0eaabee92a
a1u2m <xxxla1u2mlxxx@gmail.com>
2023-09-07 21:17:15 +0500

    flows case 14

--- app/src/main/java/com/example/coroutinesexample/examples/flows/FlowFourteenFragment.kt
+++ app/src/main/java/com/example/coroutinesexample/examples/flows/FlowFourteenFragment.kt
@@ -4,9 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.example.coroutinesexample.R
 import com.example.coroutinesexample.databinding.FragmentFlowFourteenBinding
 import com.example.coroutinesexample.examples.BaseExampleFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.produceIn
+import kotlinx.coroutines.launch
 
 class FlowFourteenFragment : BaseExampleFragment() {
 
@@ -22,5 +31,28 @@ class FlowFourteenFragment : BaseExampleFragment() {
     ): View {
         binding = FragmentFlowFourteenBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener {
+            action()
+        }
+    }
+
+    @OptIn(FlowPreview::class)
+    private fun action() {
+        lifecycleScope.launch {
+            val channel = flow {
+                for (i in 1..10) {
+                    emit(i)
+                    delay(500)
+                }
+            }
+                .produceIn(CoroutineScope(Job()))
+            channel.consumeEach {
+                loggerVM.addLog(resources.getString(R.string.flowsCase14Action1, it))
+            }
+        }
     }
 }
