cb342d478690ea5b56aba20c988e46d45eab2b14
a1u2m <xxxla1u2mlxxx@gmail.com>
2023-10-24 13:24:53 +0500

    scope functions case 7

--- app/src/main/java/com/example/coroutinesexample/examples/scopefunctions/ScopeFunctionSevenFragment.kt
+++ app/src/main/java/com/example/coroutinesexample/examples/scopefunctions/ScopeFunctionSevenFragment.kt
@@ -7,6 +7,11 @@ import android.view.ViewGroup
 import com.example.coroutinesexample.R
 import com.example.coroutinesexample.databinding.FragmentScopefunctionSevenBinding
 import com.example.coroutinesexample.examples.BaseExampleFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ScopeFunctionSevenFragment : BaseExampleFragment() {
 
@@ -14,7 +19,7 @@ class ScopeFunctionSevenFragment : BaseExampleFragment() {
         resources.getString(R.string.scopeFunctionsCase8)
     }
     private lateinit var binding: FragmentScopefunctionSevenBinding
-
+    private val scope = CoroutineScope(Job())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +27,30 @@ class ScopeFunctionSevenFragment : BaseExampleFragment() {
     ): View {
         binding = FragmentScopefunctionSevenBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.startButton.setOnClickListener {
+            actionStart()
+        }
+
+        binding.cancelButton.setOnClickListener {
+            actionCancel()
+        }
+    }
+
+    private fun actionStart() {
+        scope.launch {
+            loggerVM.addLog(resources.getString(R.string.scopeFunctionsCase7Action1))
+            for (i in 0..Long.MAX_VALUE) {
+                delay(1000)
+                loggerVM.addLog(i.toString())
+            }
+        }
+    }
+
+    private fun actionCancel() {
+        scope.cancel()
     }
 }
